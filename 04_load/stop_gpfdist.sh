8cd30446f09625ebddc0340b6943dc36fd9211bb
Ryan Wei <rainmanwy@gmail.com>
2025-12-02 17:50:29 +0800

    bug fix for stop gpfdist

--- 04_load/stop_gpfdist.sh
+++ 04_load/stop_gpfdist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-GEN_PATH_NAME={1}
+GEN_PATH_NAME=${1}
 
 for i in $(ps -ef | grep gpfdist | grep -i "${GEN_PATH_NAME}" | grep -v grep | grep -v stop_gpfdist | awk -F ' ' '{print $2}'); do
-        #echo "killing ${i}"
-        kill ${i}
+  #echo "killing ${i}"
+  kill ${i}
 done
